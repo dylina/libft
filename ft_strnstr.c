191f12ec6ac7522284d8d6e1d1beb936fa5782fa
dylina <109595463+dylina@users.noreply.github.com>
2025-04-07 17:43:56 +0200

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,38 +6,45 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 11:54:44 by dgorceac          #+#    #+#             */
-/*   Updated: 2025/04/04 15:15:56 by dgorceac         ###   ########.fr       */
+/*   Updated: 2025/04/07 16:49:06 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	unsigned char	*b;
 	unsigned char	*l;
 	size_t			i;
-	size_t 			j;
+	size_t			j;
 
 	b = (unsigned char *) big;
 	l = (unsigned char *) little;
 	i = 0;
 	j = 0;
 	if (l[0] == '\0')
-			return ((char *) b);
+		return ((char *) b);
 	while (b[i] && i < len)
 	{
-		while (b[i + j] == l[j] &&(i + j < len))
+		while (b[i + j] == l[j] && (i + j) < len)
 		{
-			j++;;
+			j++;
+			if (l[j] == '\0')
+				return ((char *) &b[i]);
 		}
+		j = 0;
+		i++;
 	}
 	return (NULL);
 }
-int	main(void)
-{
-	const char big[] = "hello";
-	const char little[] = "ll";
-	printf("%p caracterul: %c\n", ft_strnstr(big, little, 4));
-	return (0);
-}
+// int	main(void)
+// {
+// 	const char big[] = "hello";
+// 	const char little[] = "ll";
+// 	unsigned char *c;
+
+// 	c = (unsigned char *) ft_strnstr;
+// 	printf("%p\n", ft_strnstr(big, little, 4));
+// 	return (0);
+// }
