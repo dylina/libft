b234ded7b6d1079e7099160af8cf000c310edb04
dylina <109595463+dylina@users.noreply.github.com>
2025-04-08 10:00:29 +0200

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 17:02:26 by dgorceac          #+#    #+#             */
-/*   Updated: 2025/04/04 09:06:12 by dgorceac         ###   ########.fr       */
+/*   Updated: 2025/04/08 09:56:40 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (i < n && (s1[i] || s2[i]))
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 		i++;
 	}
 	return (0);
