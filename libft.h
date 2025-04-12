4724f7399066df7704a05715a96176a829c6bcd1
dylina <109595463+dylina@users.noreply.github.com>
2025-04-12 12:27:18 +0200

    Add files via upload

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 15:49:44 by dgorceac          #+#    #+#             */
-/*   Updated: 2025/04/12 10:36:33 by dgorceac         ###   ########.fr       */
+/*   Updated: 2025/04/12 12:20:04 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
