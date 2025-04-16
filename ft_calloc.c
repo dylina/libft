b48617336f3695e2dc86bd8d959923e76612c247
dylina <109595463+dylina@users.noreply.github.com>
2025-04-16 17:33:12 +0200

    Add files via upload

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 17:46:34 by dgorceac          #+#    #+#             */
-/*   Updated: 2025/04/08 10:50:58 by dgorceac         ###   ########.fr       */
+/*   Updated: 2025/04/16 17:26:23 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	*ft_calloc(size_t count, size_t size)
 
 	i = 0;
 	total_size = count * size;
+	if (count > (size_t)-1 / size)
+		return (NULL);
 	ptr = malloc(total_size);
 	if (!ptr)
 		return (NULL);
